80e2591dbd241a5e81d41e5e0a49088542a80de7
CarolinaSanches24 <carolmosilva34@gmail.com>
2026-04-21 14:33:33 -0300

    :config: add react-router

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { Home } from "./pages/Home/Home";
+import { About } from "./pages/About/About";
+import { BrowserRouter, Route, Routes, Navigate } from "react-router-dom";
+
 
 function App() {
   return (
     <div className="App">
-     <h1>MiniBlog</h1>
+      <BrowserRouter>
+        <h1>MiniBlog</h1>
+        <Routes>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/about" element={<About/>}></Route>
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
